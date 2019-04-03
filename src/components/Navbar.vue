c4894ca9a6728aaed388ba7bef9fe0e29fc14575
Zack <zackaryeaton@gmail.com>
2019-04-03 15:45:53 -0500

    Nav bar formatting
    Need to update materialize and install the code to get the mobile hamburger menu working ^_^

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,81 @@
  <template>
   <nav>
-      <div class="nav-wrapper green">
-        <div class="container">
-          <router-link to="/" class="brand-logo left hide-on-med-and-down">OV Photography</router-link>
-          <ul class="right hide-on-med-and-down">
-            <li v-if="isLoggedIn"><span class="email black-text">{{currentUser}}</span></li>
-            <li v-if="isLoggedIn"><router-link to="/">Dashboard</router-link></li>
-            <li v-if="isLoggedIn"><router-link to="/customers">Customers</router-link></li>
-            <li v-if="isLoggedIn"><router-link to="/projects">Projects</router-link></li>
-            <li v-if="!isLoggedIn"><router-link to="/login">Login</router-link></li>
-            <li v-if="!isLoggedIn"><router-link to="/register">Register</router-link></li>
-             <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Logout</button></li>
-          </ul>
-
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/" class="brand-logo left hide-on-med-and-down">OV Photography</router-link>
+        <ul class="right hide-on-med-and-down">
+          <li v-if="isLoggedIn">
+            <span class="email black-text">{{currentUser}}</span>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/">
+              <i class="fa fa-chart-line"></i>
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/customers">
+              <i class="fa fa-users"></i>
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/projects">
+              <i class="fa fa-images"></i>
+            </router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/login">
+              Login
+              <i class="fa fa-sign-in-alt"></i>
+            </router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/register">
+              Register
+              <i class="fa fa-home"></i>
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <button v-on:click="logout" class="btn black">Logout</button>
+          </li>
+        </ul>
+        <div>
         <ul class="side-nav hide-on-large-only" id="mobile-demo">
-            <li v-if="isLoggedIn"><router-link to="/"><i class="fa fa-home"></i></router-link></li>
-            <li v-if="!isLoggedIn"><router-link to="/login">Login</router-link></li>
-            <li v-if="!isLoggedIn"><router-link to="/register">Register</router-link></li>
-            <li><a href="/" class="divider"></a></li>
-            <li v-if="isLoggedIn"><button v-on:click="logout" class="btn black">Log Out</button></li>
-
-          </ul>
+          <li v-if="isLoggedIn">
+            <router-link to="/">
+              <i class="fa fa-home"></i>
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/customers">
+              <i class="fa fa-users"></i>
+            </router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <router-link to="/projects">
+              <i class="fa fa-images"></i>
+            </router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/login">Login</router-link>
+          </li>
+          <li v-if="!isLoggedIn">
+            <router-link to="/register">Register</router-link>
+          </li>
+          <li v-if="isLoggedIn">
+            <button v-on:click="logout" class="btn black">Log Out</button>
+          </li>
+        </ul>
         </div>
       </div>
-    </nav>
-  </template>
+    </div>
+  </nav>
+</template>
 
 
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
 export default {
-  name: 'navbar',
+  name: "navbar",
   data() {
     return {
       isLoggedIn: false,
