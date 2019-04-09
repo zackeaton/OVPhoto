a7cd0c5ba76e88553c37e710e90332398ddb4723
PSBrackin <gv4psb@gmail.com>
2019-04-09 18:54:21 -0500

    setup routing for pricing page
    Created base pricing page

---
+++ src/components/PriceProject.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="price-project">
+    <h3>Price Project</h3>
+    <div class="row">
+      <form @submit.prevent="updateProject" class="col s12">
+        <div class="row">
+          <div class="input-field col s 12">
+            <input disabled type="text" v-model="project_id" required>
+            <label>Project ID#</label>
+          </div>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+        <router-link to="/projects" class="btn grey">Cancel</router-link>
+      </form>
+    </div>
+  </div>
+</template>
