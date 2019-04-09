4db5936ef3e9d653bba299c45a4b8aff44d2c5ea
PSBrackin <gv4psb@gmail.com>
2019-04-09 17:08:59 -0500

    date
    date pick

--- src/components/NewProject.vue
+++ src/components/NewProject.vue
@@ -11,13 +11,13 @@
         </div>
         <div class="row">
           <div class="input-field col s 12">
-            <input type="text" v-model="consult_date" required>
+            <input type="date" class='datepicker' v-model="consult_date" required>
             <label>Consult Date</label>
           </div>
         </div>
         <div class="row">
           <div class="input-field col s 12">
-            <input type="text" v-model="shoot_date" required>
+            <input type="date" class='datepicker' v-model="shoot_date" required>
             <label>Shoot Date</label>
           </div>
         </div>
