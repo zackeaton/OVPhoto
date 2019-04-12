9168bc66c4adadd3c28b668388bccae0fc49876d
Zack <zackaryeaton@gmail.com>
2019-04-12 11:18:11 -0500

    changed PriceProject Vue to invoice template
    changed PriceProject Vue to invoice template

--- src/components/PriceProject.vue
+++ src/components/PriceProject.vue
@@ -1,45 +1,76 @@
 <template>
-  <div id="price-project">
-    <h3>Price Project</h3>
-    <div class="row">
-      <form @submit.prevent="updateProject" class="col s12">
+    <div class="invoice-box">
+  <table cellpadding="0" cellspacing="0">
+    <tr class="top">
+      <td colspan="4">
+        <table>
+          <tr>
+            <td class="title">
+              <img src="http://static1.squarespace.com/static/5982735ae6f2e1038a293da0/t/5a47f502e4966b19e3e6d4e3/1514665395643/ov1.jpg?format=1000w" style="width:100%; max-width:300px;">
+            </td>
 
-        <div class="row">
-          <div class="input-field col s 12">
-            <input disabled type="text" v-model="project_id" required>
-            <label>Project ID#</label>
-          </div>
-        </div>
+            <td>
+              Invoice #: 123<br> Created: January 1, 2015<br> Due: February 1, 2015
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
 
-        <div class="row">
-          <div class="input-field col s 12">
-            <input type="number" step='.01' v-model="main_hourly" required>
-            <label>Main Shooter Hourly Rate</label>
-          </div>
-        </div>
+    <tr class="information">
+      <td colspan="4">
+        <table>
+          <tr>
+            <td>
+              OV Photography<br> 1 University Blvd<br> St. Louis, MO 63121
+            </td>
 
-        <div class="row">
-          <div class="input-field col s 12">
-            <input type="number" step='.01' v-model="shoot_hours" required>
-            <label>Project Shoot Hours</label>
-          </div>
-        </div>
+            <td>
+              Acme Corp.<br> John Doe<br> john@example.com
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
 
-        <div class="row">
-          <div class="input-field col s 12">
-            <input type="number" step='.01' v-model="edit_hours" required>
-            <label>Project Edit Hours</label>
-          </div>
-        </div>
+    <tr class="heading">
+      <td colspan="3">Payment Method</td>
+      <td>Check #</td>
+    </tr>
 
-        
+    <tr class="details">
+      <td colspan="3">Check</td>
+      <td>1000</td>
+    </tr>
 
-        <button v-on:click='calcTotal' class="btn">Calcuate Totals</button>
-        <router-link to="/projects" class="btn grey">Cancel</router-link>
-      </form>
-    </div>
-  </div>
+    <tr class="heading">
+      <td>Item</td>
+      <td>Unit Cost</td>
+      <td>Quantity</td>
+      <td>Price</td>
+    </tr>
+
+    <tr class="item" v-for="item in items">
+      <td><input v-model="item.description" /></td>
+      <td>$<input type="number" v-model="item.price" /></td>
+      <td><input type="number" v-model="item.quantity" /></td>
+      <td>${{ item.price * item.quantity | currency }}</td>
+    </tr>
+
+    <tr>
+      <td colspan="4">
+        <button class="btn-add-row" @click="addRow">Add row</button>
+      </td>
+    </tr>
+
+    <tr class="total">
+      <td colspan="3"></td>
+      <td>Total: ${{ total | currency }}</td>
+    </tr>
+  </table>
+</div>
 </template>
+
 
 <script>
 import db from "./firebaseInit.js";
