059ea3ea60b256852026ee3c3d16e12a636dec37
PSBrackin <gv4psb@gmail.com>
2019-04-14 20:34:32 -0500

    updated styling for page

--- src/components/PriceProject.vue
+++ src/components/PriceProject.vue
@@ -15,7 +15,6 @@
           </tr>
         </table>
       </td>
-    </tr>
 
     <tr class="information">
       <td colspan="4">
@@ -42,17 +41,21 @@
       <td colspan="3">Check</td>
       <td>1000</td>
     </tr>
+  </table>
 
-    <tr class="heading">
+  <table>
+    <thead>
+      <tr>
       <td>Item</td>
       <td>Unit Cost</td>
       <td>Quantity</td>
       <td>Price</td>
-    </tr>
+      </tr>
+    </thead>
 
     <tr class="item" v-for="item in items">
       <td><input v-model="item.description" /></td>
-      <td>$<input type="number" v-model="item.price" /></td>
+      <td><input type="number" min='0' pattern="^\d*(\.\d{0,2})?$" step='.01' v-model="item.price" /></td>
       <td><input type="number" v-model="item.quantity" /></td>
       <td>${{ item.price * item.quantity}}</td>
     </tr>
@@ -80,13 +83,13 @@
 import db from "./firebaseInit.js";
 export default {
   name: "price-project",
-  el: 'ex1',
+  //el: 'ex1',
   data() {
     return {
       items: [
-      { description: "Website design", quantity: 1, price: 300 },
-      { description: "Website design", quantity: 1, price: 75 },
-      { description: "Website design", quantity: 1, price: 10 }
+      { description: "Shoot Location", quantity: 1, price: 300 },
+      { description: "Photographers", quantity: 1, price: 75 },
+      { description: "Wardrobe Changes", quantity: 1, price: 10 }
     ]
     };
   },
@@ -103,10 +106,118 @@ export default {
       this.items.push({ description: "", quantity: 1, price: 0 });
     }
   },
- /* filters: {
+ filters: {
     currency(value) {
       return value.toFixed(2);
     }
-  } */
+  }
 };
 </script>
+
+<style>
+.invoice-box {
+  max-width: 800px;
+  margin: auto;
+  padding: 30px;
+  border: 1px solid #eee;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 24px;
+  font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+  color: #555;
+}
+
+.invoice-box table {
+  width: 100%;
+  line-height: inherit;
+  text-align: left;
+}
+
+.invoice-box table td {
+  padding: 5px;
+  vertical-align: top;
+}
+
+.invoice-box table tr td:nth-child(n + 2) {
+  text-align: right;
+}
+
+.invoice-box table tr.top table td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.top table td.title {
+  font-size: 45px;
+  line-height: 45px;
+  color: #333;
+}
+
+.invoice-box table tr.information table td {
+  padding-bottom: 40px;
+}
+
+.invoice-box table tr.heading td {
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-weight: bold;
+}
+
+.invoice-box table tr.details td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.item td {
+  border-bottom: 1px solid #eee;
+}
+
+.invoice-box table tr.item.last td {
+  border-bottom: none;
+}
+
+.invoice-box table tr.item input {
+  padding-left: 5px;
+}
+
+.invoice-box table tr.item td:first-child input {
+  margin-left: -5px;
+  width: 100%;
+}
+
+.invoice-box table tr.total td:nth-child(2) {
+  border-top: 2px solid #eee;
+  font-weight: bold;
+}
+
+.invoice-box input[type="number"] {
+  width: 60px;
+}
+
+@media only screen and (max-width: 600px) {
+  .invoice-box table tr.top table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+
+  .invoice-box table tr.information table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+}
+
+/** RTL **/
+.rtl {
+  direction: rtl;
+  font-family: Tahoma, "Helvetica Neue", "Helvetica", Helvetica, Arial,
+    sans-serif;
+}
+
+.rtl table {
+  text-align: right;
+}
+
+.rtl table tr td:nth-child(2) {
+  text-align: left;
+}
+</style>
