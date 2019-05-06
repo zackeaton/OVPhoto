b7e19ea6df6084177f2d2562ae09586b345b7a25
Zack <zackaryeaton@gmail.com>
2019-05-06 17:13:54 -0500

    updated navbar routing

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="nav-wrapper">
       <div class="container">
-        <router-link to="/tasks" class="brand-logo left hide-on-med-and-down">OV Photography</router-link>
+        <router-link to="/" class="brand-logo left hide-on-med-and-down">OV Photography</router-link>
         <ul class="right hide-on-med-and-down">
           <li v-if="isLoggedIn">
             <span class="email black-text">{{currentUser}}</span>
