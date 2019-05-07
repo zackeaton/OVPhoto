37ad0ab96c4a4fdc0818e50e43bca5d45cd83c18
PSBrackin <gv4psb@gmail.com>
2019-05-06 19:12:24 -0500

    basic help page started

---
+++ src/components/contextHelp.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="context-help">
+    
+    <div id='tasks-help'>
+        This page is a view of all the tasks in the system. 
+        To add a new task, click the + in the bottom rigth corner.
+        To expand a task, click the folder icon in the task name row. 
+        This will take you to an expanded view with the task name, due date,
+        and project name. You can also delete or edit a task from this page.
+    </div>
+
+  </div>
+</template>
