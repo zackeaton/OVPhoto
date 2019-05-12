2a9c0bcbab48dc3f0f485a7e4d7559857cd9be0a
Zack <zackaryeaton@gmail.com>
2019-05-12 15:17:09 -0500

    removed project price

--- src/components/NewProject.vue
+++ src/components/NewProject.vue
@@ -60,7 +60,8 @@
             <input type="text" v-model="shoot_location" required>
             <label></label>
           </div>
-        </div>Invoice Cost:
+        </div>
+       <!--  Invoice Cost:
         <div class="row">
           <div class="input-field col s 12">
             <input type="number" v-model="invoice_cost">
@@ -72,7 +73,8 @@
             <input type="number" v-model="dept_cost">
             <label></label>
           </div>
-        </div>Deposit Status:
+        </div> -->
+        Deposit Status:
         <div class="row">
           <div class="input-field col s 12">
             <select v-model="dept_status" required>
