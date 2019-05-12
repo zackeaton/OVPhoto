dc3818c054eff8fe125600eebe6bcf2d8a5f4c47
PSBrackin <gv4psb@gmail.com>
2019-05-12 14:57:36 -0500

    updated image for new and edit invoice

--- src/components/EditInvoice.vue
+++ src/components/EditInvoice.vue
@@ -87,9 +87,9 @@
               <tr>
                 <td class="title">
                   <img
-                    src="http://static1.squarespace.com/static/5982735ae6f2e1038a293da0/t/5a47f502e4966b19e3e6d4e3/1514665395643/ov1.jpg?format=1000w"
-                    style="width:100%; max-width:300px;"
-                  >
+                  src="https://images.squarespace-cdn.com/content/5982735ae6f2e1038a293da0/1555203288932-FV7FLB6AUVDBLVA70EO7/transparent+background+copy.png?content-type=image%2Fpng"
+                  style="width:100%; max-width:300px;"
+                >
                 </td>
 
                 <td>
